48938806b5dded2a8e0a90e9a060d95a40c1f437
maykawlin <maykawlin@hotmail.com>
2022-04-18 22:55:18 -0300

    Creating the files necessary to make the frontend. There is the 'test' file to test some layouts of the deployment.

---
+++ test.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+
+with st.sidebar:
+    st.title('-----------Menu-----------')
+    st.text("")
+    st.text("")
+    add_selectbox = st.radio(
+        "Choose one:",
+        ("Home", "Neural Network", "Classificator")
+    )
+
+if add_selectbox == 'Home':
+    st.write('Home')
+elif add_selectbox == 'Neural Network':
+    st.write('Neural Network')
+elif add_selectbox == 'Classificator':
+    st.title('Classificator')
+    st.write("Drag into the box or click in 'browse files' to pick a file and classify in dog or cat.")
+    st.file_uploader('',accept_multiple_files=True)
+
+
+else:
+    pass
